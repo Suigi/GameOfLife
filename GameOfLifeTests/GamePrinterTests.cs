4db7971eced183a333c782e29ea70e9f9c689c7d
Daniel Ranner <Daniel.Ranner@pbs.one>
2021-02-26 16:12:50 +0100

    [red] we want to be able to print a game

---
+++ GameOfLifeTests/GamePrinterTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using GameOfLife;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLifeTests
+{
+    [TestClass]
+    public class GamePrinterTests
+    {
+        [TestMethod]
+        public void print_empty_grid_for_empty_game()
+        {
+            var printer = new GamePrinter();
+            var emptyGame = new Game(2,2);
+
+            var result = printer.Print(emptyGame);
+
+            result.Should().Be("__\n__\n__");
+        }
+    }
+
+    public class GamePrinter
+    {
+        public string Print(Game game)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

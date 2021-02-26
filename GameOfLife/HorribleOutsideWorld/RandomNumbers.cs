7737d59227ec1a0c7c33dde0f33f3640035b58e8
Daniel Ranner <Daniel.Ranner@pbs.one>
2021-02-26 16:54:17 +0100

    [stuff] created random numbers relying on System.Random in HorribleOutsideWorld

---
+++ GameOfLife/HorribleOutsideWorld/RandomNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameOfLife.HorribleOutsideWorld
+{
+    public class RandomNumbers
+    {
+        public static IEnumerable<double> FromSeed(int seed)
+        {
+            var random = new Random(seed);
+            while (true)
+            {
+                yield return random.NextDouble();
+            }
+            // ReSharper disable once IteratorNeverReturns
+        }
+    }
+}

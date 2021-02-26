5c7acf5bf315982560a05aa71ca50b6594083f24
Daniel Ranner <Daniel.Ranner@pbs.one>
2021-02-26 17:31:40 +0100

    [stuff] creating some load

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -8,9 +8,38 @@ namespace GameOfLife
     {
         static void Main(string[] args)
         {
+            //RunInteractive();
+            RunBig(1000, 100, 100);
+        }
+
+        private static void RunBig(int iterations = 1_000, int rows = 100, int columns = 100)
+        {
             var stable = false;
             var game =
-                new GameSeeder(RandomNumbers.FromSeed(new Random().Next()), 0.2).Create(40, 40);
+                new GameSeeder(RandomNumbers.FromSeed(new Random().Next()), 0.2).Create(
+                    rows,
+                    columns
+                );
+            var currentIteration = 0;
+            while (!stable)
+            {
+                currentIteration++;
+                if (currentIteration % 100 == 0)
+                {
+                    Console.WriteLine($"Current iteration: {currentIteration}, {game.AliveCells.Count()} cells alive.");
+                }
+                game = game.Next();
+            }
+
+            Console.WriteLine(
+                $"Alive cells after {iterations} iterations: {game.AliveCells.Count()}"
+            );
+        }
+
+        private static void RunInteractive()
+        {
+            var stable = false;
+            var game = new GameSeeder(RandomNumbers.FromSeed(new Random().Next()), 0.2).Create(40, 40);
             var printer = new GamePrinter();
             while (!stable)
             {
@@ -24,7 +53,7 @@ namespace GameOfLife
                 {
                     stable = true;
                 }
-                
+
                 Console.ReadLine();
             }
 

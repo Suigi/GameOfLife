f99562a36344bde23e03f8f77bee74d639b31374
Daniel Ranner <Daniel.Ranner@pbs.one>
2021-02-26 16:36:22 +0100

    [red] we want to be able to randomly seed games

---
+++ GameOfLifeTests/GameSeederTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using GameOfLife;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLifeTests
+{
+    [TestClass]
+    public class GameSeederTests
+    {
+        [TestMethod]
+        public void seeder_creates_a_new_game()
+        {
+            var seeder = new GameSeeder();
+            seeder.Create().Should().BeOfType<Game>();
+        }
+    }
+
+    public class GameSeeder
+    {
+        public Game Create()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

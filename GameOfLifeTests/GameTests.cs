9d341ac8982655fdb491b8eda1d1b215fe5c95b1
Daniel Ranner <Daniel.Ranner@pbs.one>
2021-02-26 14:08:25 +0100

    [red] games should have rows and columns

--- GameOfLifeTests/GameTests.cs
+++ GameOfLifeTests/GameTests.cs
@@ -9,13 +9,32 @@ namespace GameOfLifeTests
         [TestMethod]
         public void a_new_game_has_dead_cells()
         {
-            var game = new Game();
-            game.IsCellAlive(2, 2).Should().BeFalse();
+            var newGame = new Game(10, 10);
+            newGame.IsCellAlive(2, 2).Should().BeFalse();
         }
+
+        [TestMethod]
+        public void a_game_retains_its_size_in_rows_and_columns()
+        {
+            var game = new Game(2, 3);
+            game.Rows.Should().Be(2);
+            game.Columns.Should().Be(3);
+        }
+
     }
 
     public class Game
     {
+        public Game(int rows, int columns)
+        {
+        }
+
+        public int Rows
+        {
+            get { throw new System.NotImplementedException(); }
+            set { throw new System.NotImplementedException(); }
+        }
+
         public bool IsCellAlive(int i, int i1)
         {
             return false;

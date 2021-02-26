146262be53c4ae3a54668452c245d8b6f82be281
Daniel Ranner <Daniel.Ranner@pbs.one>
2021-02-26 14:06:04 +0100

    We can create a new game

--- GameOfLifeTests/GameTests.cs
+++ GameOfLifeTests/GameTests.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GameOfLifeTests
@@ -5,5 +6,19 @@ namespace GameOfLifeTests
     [TestClass]
     public class GameTests
     {
+        [TestMethod]
+        public void a_new_game_has_dead_cells()
+        {
+            var game = new Game();
+            game.IsCellAlive(2, 2).Should().BeFalse();
+        }
+    }
+
+    public class Game
+    {
+        public bool IsCellAlive(int i, int i1)
+        {
+            return false;
+        }
     }
 }

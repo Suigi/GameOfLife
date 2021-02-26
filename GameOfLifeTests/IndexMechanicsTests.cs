839b07f892b3547682dda814fae567809e0e6b5a
Daniel Ranner <Daniel.Ranner@pbs.one>
2021-02-26 14:57:15 +0100

    [red/compile error] we want to be able to calculate all neighbor indexes - starting a new test class

---
+++ GameOfLifeTests/IndexMechanicsTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLifeTests
+{
+    [TestClass]
+    public class IndexMechanicsTests
+    {
+        [TestMethod]
+        public void an_index_has_eight_neighbors()
+        {
+            IndexMechanics.Neighbors((2, 2)).Should().HaveCount(8);
+        }
+    }
+}

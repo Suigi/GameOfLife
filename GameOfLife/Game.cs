3b71a04177d177b98fe6b47a79c416c3399e7aee
Daniel Ranner <Daniel.Ranner@pbs.one>
2021-02-26 16:04:22 +0100

    [refactor] moved Game class to main project

---
+++ GameOfLife/Game.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameOfLife
+{
+    public class Game
+    {
+        private readonly (int row, int colum)[] seed;
+
+        public Game(int rows, int columns, params (int row, int colum)[] seed)
+        {
+            this.seed = seed;
+            Rows = rows;
+            Columns = columns;
+        }
+
+        public int Rows { get; }
+
+        public int Columns { get; }
+
+        public bool IsCellAlive(int row, int column)
+        {
+            CheckBounds(row, column);
+
+            return seed.Contains((row, column));
+        }
+
+        private void CheckBounds(int row, int column)
+        {
+            if (row < 0 || row >= Rows || column < 0 || column >= Columns)
+            {
+                throw new IndexOutOfRangeException($"Game index ({row},{column}) is out of bounds");
+            }
+        }
+
+        public Game Next()
+        {
+            List<(int, int)> updatedState = new List<(int, int)>();
+            foreach (var index in AllIndices())
+            {
+                var numberOfAliveNeighbors = NumberOfAliveNeighbors(index);
+                if (numberOfAliveNeighbors == 3
+                    || (numberOfAliveNeighbors == 2 && IsCellAlive(index.row, index.column)))
+                {
+                    updatedState.Add(index);
+                }
+            }
+
+            return new Game(Rows, Columns, updatedState.ToArray());
+        }
+
+        private IEnumerable<(int row, int column)> AllIndices()
+        {
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int column = 0; column < Columns; column++)
+                {
+                    yield return (row, column);
+                }
+            }
+        }
+
+        private int NumberOfAliveNeighbors((int row, int colum) index)
+        {
+            var numberOfAliveNeighbors = new IndexMechanics(Rows, Columns).Neighbors(index)
+                .Count(n => IsCellAlive(n.row, n.column));
+            return numberOfAliveNeighbors;
+        }
+    }
+}
--- GameOfLifeTests/GameTests.cs
+++ GameOfLifeTests/GameTests.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using FluentAssertions;
 using GameOfLife;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -78,71 +76,6 @@ namespace GameOfLifeTests
             var initialGame = new Game(10, 10, (1, 2), (3, 2), (2, 1));
             var updatedGame = initialGame.Next();
             updatedGame.IsCellAlive(2, 2).Should().BeTrue();
-        }
-    }
-
-    public class Game
-    {
-        private readonly (int row, int colum)[] seed;
-
-        public Game(int rows, int columns, params (int row, int colum)[] seed)
-        {
-            this.seed = seed;
-            Rows = rows;
-            Columns = columns;
-        }
-
-        public int Rows { get; }
-
-        public int Columns { get; }
-
-        public bool IsCellAlive(int row, int column)
-        {
-            CheckBounds(row, column);
-
-            return seed.Contains((row, column));
-        }
-
-        private void CheckBounds(int row, int column)
-        {
-            if (row < 0 || row >= Rows || column < 0 || column >= Columns)
-            {
-                throw new IndexOutOfRangeException($"Game index ({row},{column}) is out of bounds");
-            }
-        }
-
-        public Game Next()
-        {
-            List<(int, int)> updatedState = new List<(int, int)>();
-            foreach (var index in AllIndices())
-            {
-                var numberOfAliveNeighbors = NumberOfAliveNeighbors(index);
-                if (numberOfAliveNeighbors == 3
-                    || (numberOfAliveNeighbors == 2 && IsCellAlive(index.row, index.column)))
-                {
-                    updatedState.Add(index);
-                }
-            }
-
-            return new Game(Rows, Columns, updatedState.ToArray());
-        }
-
-        private IEnumerable<(int row, int column)> AllIndices()
-        {
-            for (int row = 0; row < Rows; row++)
-            {
-                for (int column = 0; column < Columns; column++)
-                {
-                    yield return (row, column);
-                }
-            }
-        }
-
-        private int NumberOfAliveNeighbors((int row, int colum) index)
-        {
-            var numberOfAliveNeighbors = new IndexMechanics(Rows, Columns).Neighbors(index)
-                .Count(n => IsCellAlive(n.row, n.column));
-            return numberOfAliveNeighbors;
         }
     }
 }

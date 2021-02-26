a43d09a1f9798c1266f37b2ab56a5361f1dd7de1
Daniel Ranner <Daniel.Ranner@pbs.one>
2021-02-26 16:23:15 +0100

    [red] new test for games with alive cells

--- GameOfLifeTests/GamePrinterTests.cs
+++ GameOfLifeTests/GamePrinterTests.cs
@@ -29,6 +29,26 @@ _____
             result.Should().EndWith(Environment.NewLine);
         }
 
+        [TestMethod]
+        public void alive_cells_are_printed_with_an_x()
+        {
+            var printer = new GamePrinter();
+            var emptyGame = new Game(5,5, (0,0),(1,1),(2,2),(3,3),(4,4));
+
+            var result = printer.Print(emptyGame);
+
+            AssertPrintout(result,
+                @"
+x____
+_x___
+__x__
+___x_
+____x
+"
+            );
+        }
+
+
         private void AssertPrintout(string actual, string expected)
         {
             actual.Trim().Should().Be(expected.Trim());

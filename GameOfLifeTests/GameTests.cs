a834426667819e9cfcfb43ac8722b44dea5e16ba
Daniel Ranner <Daniel.Ranner@pbs.one>
2021-02-26 14:33:30 +0100

    [refactor] renamed tests to better reflect what they are checking

--- GameOfLifeTests/GameTests.cs
+++ GameOfLifeTests/GameTests.cs
@@ -16,8 +16,8 @@ namespace GameOfLifeTests
         }
 
         [DataTestMethod]
-        [DataRow(2, 1, "(2,1)", DisplayName = "row is at exact row count")]
-        [DataRow(1, 2, "(1,2)", DisplayName = "column is at exact column count")]
+        [DataRow(2, 1, "(2,1)", DisplayName = "row index equal to number of rows it not allowed")]
+        [DataRow(1, 2, "(1,2)", DisplayName = "column index equal to number of columns it not allowed")]
         [DataRow(-1, 1, "(-1,1)", DisplayName = "negative row index is not allowed")]
         public void cells_outside_of_the_bounds_of_the_game_cannot_be_accessed(int row, int column,
             string formattedIndex)

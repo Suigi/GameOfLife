140f5aabf97991d06e0bc8ea556833c04f84a797
Daniel Ranner <Daniel.Ranner@pbs.one>
2021-02-26 14:58:33 +0100

    [red] test is now compiling

--- GameOfLifeTests/IndexMechanicsTests.cs
+++ GameOfLifeTests/IndexMechanicsTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GameOfLifeTests
 {
@@ -9,6 +11,14 @@ namespace GameOfLifeTests
         public void an_index_has_eight_neighbors()
         {
             IndexMechanics.Neighbors((2, 2)).Should().HaveCount(8);
+        }
+    }
+
+    public static class IndexMechanics
+    {
+        public static IEnumerable<(int,int)> Neighbors((int, int) valueTuple)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }

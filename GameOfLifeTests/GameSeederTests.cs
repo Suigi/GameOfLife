eb67842200faa1d3f4b7bf8efc7e4a9bf2ace225
Daniel Ranner <Daniel.Ranner@pbs.one>
2021-02-26 16:54:50 +0100

    [refactor] moved GameSeeder to main project

--- GameOfLifeTests/GameSeederTests.cs
+++ GameOfLifeTests/GameSeederTests.cs
@@ -35,38 +35,4 @@ namespace GameOfLifeTests
             // ReSharper disable once IteratorNeverReturns
         }
     }
-
-    public class GameSeeder
-    {
-        private readonly IEnumerator<double> randomEnumerator;
-        private readonly double probability;
-        
-        public GameSeeder(IEnumerable<double> numbers, double probability)
-        {
-            this.probability = probability;
-            randomEnumerator = numbers.GetEnumerator();
-        }
-
-        public Game Create(int rows, int columns)
-        {
-            var seed = new List<(int, int)>();
-            for (int row = 0; row < rows; row++)
-            {
-                for (int column = 0; column < columns; column++)
-                {
-                    if (IsNextAlive())
-                    {
-                        seed.Add((row,column));
-                    }
-                }
-            }
-            return new Game(rows, columns, seed.ToArray());
-        }
-
-        private bool IsNextAlive()
-        {
-            randomEnumerator.MoveNext();
-            return randomEnumerator.Current > probability;
-        }
-    }
 }

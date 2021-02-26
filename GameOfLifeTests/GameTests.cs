1f3e470819f39cefe44468dcf47ecf84efb64f3b
Daniel Ranner <Daniel.Ranner@pbs.one>
2021-02-26 14:15:57 +0100

    [red] add test checking for out of bounds errors

--- GameOfLifeTests/GameTests.cs
+++ GameOfLifeTests/GameTests.cs
@@ -1,3 +1,4 @@
+using System;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,6 +14,17 @@ namespace GameOfLifeTests
             game.Rows.Should().Be(2);
             game.Columns.Should().Be(3);
         }
+
+        [TestMethod]
+        public void cells_outside_of_the_bounds_of_the_game_cannot_be_accessed()
+        {
+            var game = new Game(2, 2);
+            
+            Action act = () => game.IsCellAlive(2, 2);
+
+            act.Should().Throw<IndexOutOfRangeException>().WithMessage("Game index (2,2) is out of bounds");
+        }
+
 
         [TestMethod]
         public void a_new_game_has_dead_cells()
